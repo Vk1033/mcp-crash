c19e6b06caf27138511261807cd6efdb04e157c4
Vk1033 <kinkarvansh@gmail.com>
2026-05-24 14:16:26 +0000

    feat: integrate mcp client and nvidia chat model into main execution flow

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import asyncio
 
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+from langchain.agents import create_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+llm = ChatNVIDIA(model="mistralai/mistral-nemotron")
+
+stdio_server_params = StdioServerParameters(
+    command="uv",
+    args=["run", "servers/math_server.py"],
+)
+
 
 async def main():
     print("Hello from mcp-crash!")
